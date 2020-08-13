38f91fb79e222c215a0e55f26cea3a2fb5f612eb
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-08-13 20:52:22 +0700

    done sstf

--- assets/script.js
+++ assets/script.js
@@ -19,8 +19,11 @@ self.addEventListener('DOMContentLoaded', () => {
         let trackValue = Track.value.split(' ')
         trackValue.unshift(Head.value)
         
-        //calculateFIFO
+        //calculate FIFO
         calculateFIFO(FIFO, [...trackValue])
+
+        //calculate SSTF
+        calculateSSTF(SSTF, [...Track.value.split(' ')], Head.value)
     })
 })
 
@@ -61,6 +64,77 @@ const calculateFIFO = (id, track) => {
         FIFOTimeDeviation['timeDeviationString'],
         'FIFO'
     )
+}
+
+const calculateSSTF = (id, domSequence, domHead) => {
+    arr = domSequence
+    start = parseInt(domHead)
+
+    console.log(arr, start)
+
+    if (arr.length === 0) {
+        alert("sequence kosong!")
+    } else {
+        var nodeDistance = new Array(arr.length)
+        var nodeAccessed = new Array(arr.length)
+
+        var i = 0
+        for (i = 0; i < arr.length; i++) {
+            arr[i] = parseInt(arr[i])
+            nodeDistance[i] = 0
+            nodeAccessed[i] = false
+        }
+
+        var seek_count = 0
+        var seek_sequence = new Array(arr.length+1)
+
+        for (i = 0; i < arr.length ; i++) {
+            seek_sequence[i] = start
+
+            // Calculates difference of each
+            // track number with the head position
+            var j = 0
+            for(j = 0; j < arr.length; j++){
+                nodeDistance[j] = Math.abs(arr[j] - start)
+            }
+
+            // find unaccessed track
+            // which is at minimum distance from head
+            var index = -1
+            var minimum = Number.MAX_VALUE;
+
+            for (j = 0; j < arr.length; j++) {
+                if (!nodeAccessed[j] && minimum > nodeDistance[j]) {
+                    minimum = nodeDistance[j];
+                    index = j;
+                }
+            }
+
+            nodeAccessed[index] = true
+
+            // increase the total count
+            seek_count += nodeDistance[index]
+
+            //accessed track is now new head
+            start = arr[index]
+        }
+
+        // for last accessed track
+        seek_sequence[seek_sequence.length - 1] = start;
+
+        const SSTFTimeDeviation = timeDeviation(seek_sequence)
+
+        setChart(
+            id,
+            seek_sequence,
+            SSTFTimeDeviation['timeDeviationString'],
+            'SSTF'
+        )
+    }
+}
+
+const calculateSCAN = () => {
+    
 }
 
 const timeDeviation = track => {
